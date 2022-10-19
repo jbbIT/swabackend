b63c87facec9b728ef61dba20eebe4ba6c2775dc
jbbIT <115475053+jbbIT@users.noreply.github.com>
2022-10-19 14:34:20 +0200

    changed SERVER_PROT to PORT line 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
