b50bab988dde54d9c2f74e64a4d6076b3fe57022
jbbIT <115475053+jbbIT@users.noreply.github.com>
2022-10-19 14:23:44 +0200

    added SERVER_PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(cors());
